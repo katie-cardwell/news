43c018cbddcb9191df16f1fcb8a689274e206605
katie-cardwell <katie.cardwell@kellogg.northwestern.edu>
2020-05-20 23:43:13 +0000

    Commit

--- app.rb
+++ app.rb
@@ -3,35 +3,34 @@ require "sinatra/reloader"
 require "httparty"
 def view(template); erb template.to_sym; end
 
-get "/" do
-  ### Get the weather
-  # Evanston, Kellogg Global Hub... replace with a different location if you want
-  lat = 42.0574063
-  long = -87.6722787
-  units = "imperial" # or metric, whatever you like
-  key = "ea579953661604a97a00a0b0af0a9d5c" # replace this with your real OpenWeather API key
+get "/" do
+  ### Get the weather
+  # Evanston, Kellogg Global Hub... replace with a different location if you want
+  lat = 42.0574063
+  long = -87.6722787
 
-  # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
-  url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
+  units = "imperial" # or metric, whatever you like
+  key = "ea579953661604a97a00a0b0af0a9d5c" # replace this with your real OpenWeather API key
 
-  # make the call
-  @forecast = HTTParty.get(url).parsed_response.to_hash
-  
-    puts "It is currently #{@forecast["current"]["temp"]} degrees with #{@forecast["current"]["weather"][0]["description"]}"
-    puts "Extended forecast:"
-    day_number = 1
-    
-    for day in @forecast["daily"]
-        puts "On day #{day_number}, A high of #{day["temp"]["max"]} with #{day["weather"][0]["description"]}"
-        day_number = day_number + 1
-    end
+  # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
+  url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
 
-  ### Get the news
-  url= "https:newsapi.org/v2/top-headlines?country=us&apiKey=d43d1ad46bc04b56943a9bb321cc589b"
-  @news = HTTParty.get(url).parsed_response.to_hash
+  # make the call
+  @forecast = HTTParty.get(url).parsed_response.to_hash
+    puts "It is currently #{@forecast["current"]["temp"]} degrees with #{@forecast["current"]["weather"][0]["description"]}"
+    puts "Extended forecast:"
+    day_number = 1
+    for day in @forecast["daily"]
+        puts "On day #{day_number}, A high of #{day["temp"]["max"]} with #{day["weather"][0]["description"]}"
+        day_number = day_number + 1
+    end
+  ### Get the news
 
-    @todaysnews = @news ["articles"][0,10]
+    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=d43d1ad46bc04b56943a9bb321cc589b"
+    @news = HTTParty.get(url).parsed_response.to_hash
+    @todaysnews = @news ["articles"][0,10]
 
-    view 'news'
+  view 'news'
 
-END
+
+end
